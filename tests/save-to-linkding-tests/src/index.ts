f8fb60c1a7629c3982b2ce3df5d270f0db52e2a5
Logan Crandall <logan@accidentallycoded.com>
2023-08-22 19:36:00 -0400

    Delete tests

--- tests/save-to-linkding-tests/src/index.ts
+++
@@ -1,6 +0,0 @@
-// import shortcut from "@ios-shortcuts/save-to-linkding";
-import shortcut from "@ios-shortcuts/vite";
-import { simulator } from "@ios-shortcuts/scriptable";
-
-simulator.setupEnvironment();
-shortcut.test()
