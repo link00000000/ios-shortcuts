8179ab3eeaddd5e4aa999ff56ac8140a42b420ec
Logan Crandall <logan@accidentallycoded.com>
2023-08-26 17:15:13 -0400

    Fix invalid local packages path

--- shortcuts/save-to-linkding/src/index.ts
+++ shortcuts/save-to-linkding/src/index.ts
@@ -1,12 +1,8 @@
 import * as scriptable from "@ios-shortcuts/scriptable";
 
 async function main() {
-    const y = new TextField();
-
-    const alert = new TestGlobalClass();
-    alert.title = "Test title";
-    alert.message = "Test message";
-    alert.present();
+    const alert = new Alert();
+    alert.presentAlert();
 }
 
 main();
