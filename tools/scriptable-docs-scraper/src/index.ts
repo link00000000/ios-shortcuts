ac277a196d85f256f399bd25eb3d5a762ef48503
Logan Crandall <logan@accidentallycoded.com>
2023-08-29 10:10:45 -0400

    Move scraper to tools

---
+++ tools/scriptable-docs-scraper/src/index.ts
@@ -0,0 +1,7 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+import process from "node:process";
+
+console.dir(process.argv)
+
+// fs.writeFile(path.join(process.cwd(), "test.txt"), "Hello");
